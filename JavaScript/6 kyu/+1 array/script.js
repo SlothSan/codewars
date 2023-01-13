1c85ba49621311ed1b6eedbdd203781645da8ae3
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2023-01-13 14:12:09 +0000

    Refactor.
    Still dealing with edge cases.

--- JavaScript/6 kyu/+1 array/script.js
+++ JavaScript/6 kyu/+1 array/script.js
@@ -18,69 +18,68 @@ Invalid arrays
 
 [1, 2, 33] is invalid because 33 is not a single-digit integer */
 
+const convertToNumber = (number) => {
+  return Number(number);
+};
+
 const upArray = (arr) => {
+  //if array empty return it.
+  let leadingZeroCount = 0;
+  let max = 0; 
+  let firstArr = [];
+  let secondArr = [];
   if (arr === []) {
     return arr;
   }
-  let upArray = [];
-  let max = 0;
-  let leadingZeroCount = 0;
-  //Iterate through the array, if any values are negative or bigger than 9 return null
+  //Iterate through the array, if any values are negative or greater than 9 return null
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] < 0 || arr[i] > 9) {
-      return (upArray = null);
+      return (arr = null);
     }
-    if (max < 1 && arr[i] < 1) {
+    //detect leading zero's and count them to add them back later.
+    if(max < 1 && arr[i] < 1) {
       leadingZeroCount++;
     }
     max = arr[i];
   }
-  if (arr.length > 16) {
-    let firstArr = [];
-    let secondArr = [];
+  //If null coniditions are met return it. 
+  if (arr === null) {
+    return arr;
+  }
+  //Handle large arrays by splitting them before doing maths on them dur to int limitations
+  if (arr.length > 9) {
     for (let i = 0; i < arr.length; i++) {
-      if (i < 16) {
+      if (i <= 9) {
         firstArr.push(arr[i]);
       } else {
         secondArr.push(arr[i]);
       }
-      secondArr.join("");
-      secondArr = parseInt(secondArr);
-      for (let j = 0; j < secondArr.length; j++) {
-        firstArr.push(secondArr[j]);
-      }
-      return firstArr;
     }
-  } else {
-    arr = arr.join("");
-    arr = parseInt(arr);
-    arr++;
-    return arr;
+    secondArr = parseInt(secondArr.join("")) + 1;
+    secondArr = Array.from(String(secondArr), convertToNumber);
+    for (let j = 0; j < secondArr.length; j++) {
+      firstArr.push(secondArr[j]);
+    }
+    arr = firstArr;
+  } else  {
+    //Handle smaller arrays
+    arr = parseInt(arr.join("")) + 1;
+    console.log(typeof arr);
+    arr = Array.from(String(arr), convertToNumber);
+  }
+  //Add leading 0's back.
+  if(leadingZeroCount > 0) {
+    for(let i = 0; i < leadingZeroCount; i++) {
+      arr.unshift(0);
+    }
   }
   return arr;
-  // //Catch leading 0's
-  // //   console.log("0 Count: ", leadingZeroCount);
-  // //   console.log(arr);
-  // //   console.log(arr.join("") + 1);
-  // upArray = BigInt(arr.join(""));
-  // upArray = upArray + BigInt("1");
-  // console.log("After +1: ", upArray);
-  // upArray = upArray.toString().split("").map((number) => {return BigInt(number)});
-  // //   upArray = upArray
-  // //     .toString()
-  // //     .split("")
-  // //     .map((number) => {
-  // //       return Number(number);
-  // //     });
-
-  // upArray.pop();
-  // return upArray;
 };
 
 console.log(upArray([0, 4, 3, 2, 0]));
-console.log(
-  upArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
-);
+// console.log(
+//   upArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+// );
 // console.log(upArray([0, 7])) // Should return 08
 // console.log(
 //   upArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
@@ -88,4 +87,3 @@ console.log(
 // console.log(upArray([1, -9])); //Should return null
 // console.log(upArray([1, 10])); // should return null
 // console.log(parseInt("12345678901234567890", 10)); // WHY IS THIS 12345678901234567000 ?
-console.log(BigInt("12345678901234567890") + BigInt("1"));
