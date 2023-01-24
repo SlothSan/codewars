a9730dedd188443180ddc19f5a69d368ede46a93
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2023-01-24 21:05:15 +0000

    Completed solutions

---
+++ JavaScript/6 kyu/if you can read this/script.js
@@ -0,0 +1,63 @@
+/*Task
+You'll have to translate a string to Pilot's alphabet (NATO phonetic alphabet).
+
+Input:
+
+If, you can read?
+
+Output:
+
+India Foxtrot , Yankee Oscar Uniform Charlie Alfa November Romeo Echo Alfa Delta ?
+
+Note:
+
+There are preloaded dictionary you can use, named NATO
+The set of used punctuation is ,.!?.
+Punctuation should be kept in your return string, but spaces should not.
+Xray should not have a dash within.
+Every word and punctuation mark should be seperated by a space ' '.
+There should be no trailing whitespace*/
+const natoMapOwn = {
+  a: "Alfa",
+  n: "November",
+  b: "Bravo",
+  o: "Oscar",
+  c: "Charlie",
+  p: "Papa",
+  d: "Delta",
+  q: "Quebec",
+  e: "Echo",
+  r: "Romeo",
+  f: "Foxtrot",
+  s: "Sierra",
+  g: "Golf",
+  t: "Tango",
+  h: "Hotel",
+  u: "Uniform",
+  i: "India",
+  v: "Victor",
+  j: "Juliett",
+  w: "Whiskey",
+  k: "Kilo",
+  x: "Xray",
+  l: "Lima",
+  y: "Yankee",
+  m: "Mike",
+  z: "Zulu",
+};
+
+const to_nato = (words, natoMapOwn) => {
+  return words
+    .replace(/\s/g, "")
+    .toLowerCase()
+    .split("")
+    .map((character) => {
+      if (/[a-zA-Z]/.test(character)) {
+        return natoMapOwn[character];
+      }
+      return character;
+    })
+    .join(" ");
+};
+
+console.log(to_nato("If, you can read?", natoMapOwn));
