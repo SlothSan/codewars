6ff0f130e7aa85ca2fcac590a8bbcdeed76b1f0d
Mike Oram <Mikeoram01@gmail.com>
2023-02-07 13:20:58 +0000

    Complete solution

---
+++ JavaScript/8 kyu/polish alphabet/script.js
@@ -0,0 +1,42 @@
+/*
+There are 32 letters in the Polish alphabet: 9 vowels and 23 consonants.
+
+Your task is to change the letters with diacritics:
+
+ą -> a,
+ć -> c,
+ę -> e,
+ł -> l,
+ń -> n,
+ó -> o,
+ś -> s,
+ź -> z,
+ż -> z
+and print out the string without the use of the Polish letters.
+
+For example:
+
+"Jędrzej Błądziński"  -->  "Jedrzej Bladzinski"
+*/
+
+const correctPolishLetters = (string) => {
+  const polishMap = {
+    ą: "a",
+    ć: "c",
+    ę: "e",
+    ł: "l",
+    ń: "n",
+    ó: "o",
+    ś: "s",
+    ź: "z",
+    ż: "z",
+  };
+  return string
+    .split("")
+    .map((letter) =>
+      polishMap[letter] !== undefined ? polishMap[letter] : letter
+    )
+    .join("");
+};
+
+console.log(correctPolishLetters("Jędrzej Błądziński")); //Expected Jedrzej Bladzinski
