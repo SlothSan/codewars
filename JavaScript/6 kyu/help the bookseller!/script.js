0ff7c93b7a494e4e126bc1f5b68db7d0ba0e055f
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2023-02-25 14:16:55 +0000

    Complete solution

---
+++ JavaScript/6 kyu/help the bookseller!/script.js
@@ -0,0 +1,50 @@
+/*
+A bookseller has lots of books classified in 26 categories labeled A, B, ... Z. Each book has a code c of 3, 4, 5 or more characters. The 1st character of a code is a capital letter which defines the book category.
+
+In the bookseller's stocklist each code c is followed by a space and by a positive integer n (int n >= 0) which indicates the quantity of books of this code in stock.
+
+For example an extract of a stocklist could be:
+
+L = {"ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"}.
+or
+L = ["ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"] or ....
+You will be given a stocklist (e.g. : L) and a list of categories in capital letters e.g :
+
+M = {"A", "B", "C", "W"} 
+or
+M = ["A", "B", "C", "W"] or ...
+and your task is to find all the books of L with codes belonging to each category of M and to sum their quantity according to each category.
+
+For the lists L and M of example you have to return the string (in Haskell/Clojure/Racket/Prolog a list of pairs):
+
+(A : 20) - (B : 114) - (C : 50) - (W : 0)
+where A, B, C, W are the categories, 20 is the sum of the unique book of category A, 114 the sum corresponding to "BKWRK" and "BTSQZ", 50 corresponding to "CDXEF" and 0 to category 'W' since there are no code beginning with W.
+
+If L or M are empty return string is "" (Clojure/Racket/Prolog should return an empty array/list instead).
+
+Notes:
+In the result codes and their values are in the same order as in M.
+See "Samples Tests" for the return.
+*/
+
+const stockList = (listOfArt, listOfCat) => {
+  if (listOfArt.length < 1 || listOfCat.length < 1) return "";
+  let returnArr = [];
+  for (let i = 0; i < listOfCat.length; i++) {
+    let target = listOfCat[i],
+      sum = 0;
+    for (let j = 0; j < listOfArt.length; j++) {
+      if (listOfArt[j][0] === target) {
+        sum += Number(listOfArt[j].replace(/\D/g, ""));
+      }
+    }
+    returnArr.push(`(${target} : ${sum})`);
+    sum = 0;
+  }
+  return returnArr.join(" - ");
+};
+
+let b = ["ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"];
+let c = ["A", "B"];
+
+console.log(stockList(b, c)); //Expect "(A : 200) - (B : 1140)"
