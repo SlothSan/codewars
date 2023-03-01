4413f700d3a17ad2e5dae2bb04921e2363a63e4b
Mike Oram <Mikeoram01@gmail.com>
2023-03-01 11:41:48 +0000

    complete solution

---
+++ JavaScript/7 kyu/max-min arrays/script.js
@@ -0,0 +1,25 @@
+/* 
+In this Kata, you will be given an array of unique elements, and your task is to rearrange the values so that the first max value is followed by the first minimum, followed by second max value then second min value, etc.
+
+For example:
+
+solve([15,11,10,7,12]) = [15,7,12,10,11]
+The first max is 15 and the first min is 7. The second max is 12 and the second min is 10 and so on.
+
+More examples in the test cases.
+
+Good luck!
+*/
+
+const solve = (arr) => {
+  let returnArr = [];
+  let sortHigh = [...arr].sort((a, b) => b - a);
+  let sortLow = [...arr].sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    returnArr.push(sortHigh[i]);
+    returnArr.push(sortLow[i]);
+  }
+  return returnArr.slice(0, arr.length);
+};
+
+console.log(solve([15, 11, 10, 7, 12])); //[15,7,12,10,11]
