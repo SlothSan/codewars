3827c2d44d08798fa501a89c0849ca2e0ea89583
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2023-03-02 21:37:59 +0000

    complete solution

---
+++ JavaScript/8 kyu/rock paper scissors/script.js
@@ -0,0 +1,34 @@
+/*
+Rock Paper Scissors
+Let's play! You have to return which player won! In case of a draw return Draw!.
+
+Examples(Input1, Input2 --> Output):
+
+"scissors", "paper" --> "Player 1 won!"
+"scissors", "rock" --> "Player 2 won!"
+"paper", "paper" --> "Draw!"
+*/
+
+const rps = (p1, p2) => {
+  if (p1 === "scissors") {
+    return p2 === "scissors"
+      ? "Draw!"
+      : p2 === "rock"
+      ? "Player 2 won!"
+      : "Player 1 won!";
+  }
+  if (p1 === "rock") {
+    return p2 === "rock"
+      ? "Draw!"
+      : p2 === "paper"
+      ? "Player 2 won!"
+      : "Player 1 won!";
+  }
+  if (p1 === "paper") {
+    return p2 === "paper"
+      ? "Draw!"
+      : p2 === "scissors"
+      ? "Player 2 won!"
+      : "Player 1 won!";
+  }
+};
