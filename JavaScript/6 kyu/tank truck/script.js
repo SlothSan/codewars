9a986fe30cf846d675fea44c84fc4ac0d6556dc0
Mike Oram <Mikeoram01@gmail.com>
2023-03-08 15:54:59 +0000

    complete solution

---
+++ JavaScript/6 kyu/tank truck/script.js
@@ -0,0 +1,23 @@
+/*
+To introduce the problem think to my neighbor who drives a tanker truck. The level indicator is down and he is worried because he does not know if he will be able to make deliveries. We put the truck on a horizontal ground and measured the height of the liquid in the tank.
+
+Fortunately the tank is a perfect cylinder and the vertical walls on each end are flat. The height of the remaining liquid is h, the diameter of the cylinder base is d, the total volume is vt (h, d, vt are positive or null integers). You can assume that h <= d.
+
+Could you calculate the remaining volume of the liquid? Your function tankvol(h, d, vt) returns an integer which is the truncated result (e.g floor) of your float calculation.
+
+Examples:
+tankvol(40,120,3500) should return 1021 (calculation gives about: 1021.26992027)
+
+tankvol(60,120,3500) should return 1750
+
+tankvol(80,120,3500) should return 2478 (calculation gives about: 2478.73007973)
+*/
+const tankvol = (height, diameter, volumeTotal) => {
+  return Math.floor(
+    ((4 * volumeTotal) / (Math.pow(diameter, 2) * Math.PI)) *
+      (Math.pow(diameter / 2, 2) * Math.acos(1 - height / (diameter / 2)) -
+        (diameter / 2 - height) * Math.sqrt(height * (diameter - height)))
+  );
+};
+
+console.log(tankvol(5, 7, 3848)); //2940
