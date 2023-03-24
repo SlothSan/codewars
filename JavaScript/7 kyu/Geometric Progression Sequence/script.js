e20a39a2d9172b263170d7901a6f34c5ec5ccb97
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2023-03-24 11:17:50 +0000

    Complete solutions
    Final push to top 1%

---
+++ JavaScript/7 kyu/Geometric Progression Sequence/script.js
@@ -0,0 +1,15 @@
+/*
+In your class, you have started lessons about geometric progression. Since you are also a programmer, you have decided to write a function that will print first n elements of the sequence with the given constant r and first element a.
+
+Result should be separated by comma and space.
+
+Example
+geometricSequenceElements(2, 3, 5) == '2, 6, 18, 54, 162'
+More info: https://en.wikipedia.org/wiki/Geometric_progression
+*/
+
+const geometricSequenceElements = (a, r, n) => {
+  return [...Array(n)].map((_, i) => a * r ** i).join(", ");
+};
+
+console.log(geometricSequenceElements(2, 3, 5)); //'2, 6, 18, 54, 162'
