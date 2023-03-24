66b713dc9978c79a28f740403a21e2301feb529d
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2023-03-24 11:56:42 +0000

    complete solutions

---
+++ JavaScript/7 kyu/easy time convert/script.js
@@ -0,0 +1,23 @@
+/*
+This kata requires you to convert minutes (int) to hours and minutes in the format hh:mm (string).
+
+If the input is 0 or negative value, then you should return "00:00"
+
+Hint: use the modulo operation to solve this challenge. The modulo operation simply returns the remainder after a division. For example the remainder of 5 / 2 is 1, so 5 modulo 2 is 1.
+
+Example
+If the input is 78, then you should return "01:18", because 78 minutes converts to 1 hour and 18 minutes.
+
+Good luck! :D
+*/
+
+const timeConvert = (num) => {
+  if (num <= 0) return "00:00";
+  const min = Math.floor(num / 60);
+  const sec = num % 60;
+  const resultMin = min < 10 ? "0" + min : min;
+  const resultSec = sec < 10 ? "0" + sec : sec;
+  return `${resultMin}:${resultSec}`;
+};
+
+console.log(timeConvert(75)); //01:15
