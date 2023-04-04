9ac5146a5991f9009afeb47a51d30c23a7f92090
Mike Oram <Mikeoram01@gmail.com>
2023-04-04 12:30:36 +0100

    complete solution

---
+++ JavaScript/7 kyu/Words to sentence/script.js
@@ -0,0 +1,13 @@
+/*
+Write function which will create a string from a list of strings, separated by space.
+
+Example:
+
+["hello", "world"] -> "hello world"
+
+s
+*/
+
+const wordsToSentence = (words) => words.join(" ");
+
+console.log(wordsToSentence(["Hello", "World"])); //Hello World
