98961dcff21e51dbb3861c9924eb7f3d520ab8ec
Mike Oram <Mikeoram01@gmail.com>
2023-04-17 10:55:49 +0100

    complete solution

---
+++ JavaScript/6 kyu/Number Format/script.js
@@ -0,0 +1,12 @@
+/*
+Format any integer provided into a string with "," (commas) in the correct places.
+
+Example:
+
+numberFormat(100000); // return '100,000'
+numberFormat(5678545); // return '5,678,545'
+numberFormat(-420902); // return '-420,902'
+*/
+
+const numberFormat = (number) => number.toLocaleString();
+console.log(numberFormat(100000)); //100,000
