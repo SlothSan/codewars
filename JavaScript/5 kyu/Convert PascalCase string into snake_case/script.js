ba87cde33ace09e139c02f08b3968b9f974dcfa7
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2023-05-11 21:24:35 +0100

    complete solution

---
+++ JavaScript/5 kyu/Convert PascalCase string into snake_case/script.js
@@ -0,0 +1,16 @@
+/*
+Complete the function/method so that it takes a PascalCase string and returns the string in snake_case notation. Lowercase characters can be numbers. If the method gets a number as input, it should return a string.
+
+Examples
+"TestController"  -->  "test_controller"
+"MoviesAndBooks"  -->  "movies_and_books"
+"App7Test"        -->  "app7_test"
+1                 -->  "1"
+*/
+
+const toUnderscore = (string) =>
+  String(string).replace(/[A-Z]/g, (match, index) => {
+    return index === 0 ? match.toLowerCase() : "_" + match.toLowerCase();
+  });
+
+console.log(toUnderscore("HelloWorld")); //hello_world
