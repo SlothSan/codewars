745cf60d98033f0fec22199fcf9d4a6812804e90
Mike Oram <Mikeoram01@gmail.com>
2023-06-04 11:07:10 +0100

    Complete solution

---
+++ JavaScript/6 kyu/Count addresses grouped by state/script.js
@@ -0,0 +1,65 @@
+/*
+Write a function that can take an array of addresses as input, and return an array with the count of addresses for each state.
+
+The input objects will have the following structure:
+
+{
+  house: 1234,
+  street: "Lonely St.",
+  city: "Soldotna",
+  zipcode: 99669,
+  state: "AK"
+}
+The returned objects must have the following structure:
+
+{
+  state: "AK",
+  count: 25
+}
+The states must be ordered by first appearance.
+
+An error is expected if an input object doesn't have a state property.
+*/
+
+const count = (addresses) => {
+  let counts = {};
+  addresses.forEach((address) => {
+    if (!address.state) {
+      throw new Error("Input object doesn't have a state property");
+    }
+    if (counts[address.state]) {
+      counts[address.state]++;
+    } else {
+      counts[address.state] = 1;
+    }
+  });
+  let result = [];
+  for (let state in counts) {
+    result.push({
+      state: state,
+      count: counts[state],
+    });
+  }
+  return result;
+};
+
+console.log(
+  count([
+    { house: 1, street: "Binary St.", city: "Zero Value", state: "AK" },
+    { house: 1, street: "Binary St.", city: "Zero Value", state: "AR" },
+    { house: 1, street: "Binary St.", city: "Zero Value", state: "OR" },
+    { house: 1, street: "Binary St.", city: "Zero Value", state: "CA" },
+    { house: 1, street: "Binary St.", city: "Zero Value", state: "AK" },
+    { house: 1, street: "Binary St.", city: "Zero Value", state: "OR" },
+    { house: 1, street: "Binary St.", city: "Zero Value", state: "OR" },
+  ])
+);
+
+/*
+[
+  { state: 'AK', count: 2 },
+  { state: 'AR', count: 1 },
+  { state: 'OR', count: 3 },
+  { state: 'CA', count: 1 }
+]
+*/
