763fb2864986c44d80a5a3b59799d771e5c2b1e5
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2023-06-10 11:14:03 +0100

    Complete solution.

---
+++ JavaScript/8 kyu/Return to sanity/script.js
@@ -0,0 +1,9 @@
+/*
+This function should return an object, but it's not doing what's intended. What's wrong?
+*/
+
+const mystery = () => {
+  return { sanity: "Hello" };
+};
+
+console.log(mystery()); //{ sanity: 'Hello' }
