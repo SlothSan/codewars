8a2a96ff2fad2843e9659a939fed965588334154
Mike Oram <Mikeoram01@gmail.com>
2023-06-19 17:50:10 +0100

    Reefactor

--- JavaScript/8 kyu/easy logs/script.js
+++ JavaScript/8 kyu/easy logs/script.js
@@ -3,9 +3,7 @@ Add two logs with base X, with the value of A and B. Example log A + log B where
 */
 
 const logs = (x, a, b) => {
-  const logA = Math.log(a) / Math.log(x);
-  const logB = Math.log(b) / Math.log(x);
-  return logA + logB;
+  return Math.log(a * b) / Math.log(x);
 };
 
 console.log(logs(10, 2, 3)); //0.7781512503836434
