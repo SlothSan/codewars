109cd7e13b82a8c96f341db9c898da68a505c61f
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2023-08-24 06:57:53 +0100

    Complete solution

---
+++ JavaScript/6 kyu/Simple Sentences/script.js
@@ -0,0 +1,30 @@
+/*
+Implement a function, so it will produce a sentence out of the given parts.
+
+Array of parts could contain:
+
+words;
+commas in the middle;
+multiple periods at the end.
+Sentence making rules:
+
+there must always be a space between words;
+there must not be a space between a comma and word on the left;
+there must always be one and only one period at the end of a sentence.
+*/
+
+const makeSentence = (parts) => {
+  let resultStr = "";
+  parts.map((part, index) => {
+    if (!part.includes(".")) {
+      if (part.includes(",") || index === 0) {
+        resultStr += part;
+      } else {
+        resultStr += ` ${part}`;
+      }
+    }
+  });
+  return `${resultStr}.`;
+};
+
+console.log(makeSentence(["Hello", ",", "World", "..."]));
