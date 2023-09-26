1ca0a9589ac558474e04518c7be56239060ee7f9
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2023-09-26 09:22:06 +0100

    Complete solution.

---
+++ JavaScript/7 kyu/isEven - Bitwise Series/script.js
@@ -0,0 +1,16 @@
+/*
+Is the number even?
+If the numbers is even return true. If it's odd, return false.
+
+Oh yeah... the following symbols/commands have been disabled!
+
+use of %
+use of .even? in Ruby
+use of mod in Python
+*/
+
+const isEven = (n) => {
+  return (n & 1) === 0;
+};
+
+console.log(isEven(12));
