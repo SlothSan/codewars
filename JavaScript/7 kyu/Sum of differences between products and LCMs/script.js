025275b755e31083cb83ffa72e49677db66e9814
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2023-10-14 09:49:25 +0100

    Refactor

--- JavaScript/7 kyu/Sum of differences between products and LCMs/script.js
+++ JavaScript/7 kyu/Sum of differences between products and LCMs/script.js
@@ -20,26 +20,12 @@ Thus the result should be:
 This is a kata that I made, among other things, to let some of my trainees familiarize with the euclidean algorithm, a really neat tool to have on your belt ;)
 */
 
-function gcd(a, b) {
-  if (b === 0) return a;
-  return gcd(b, a % b);
-}
-
-function lcm(a, b) {
-  if (a === 0 || b === 0) return 0;
-  return (a * b) / gcd(a, b);
-}
-
 function sumDifferencesBetweenProductsAndLCMs(pairs) {
-  let result = 0;
-
-  for (let i = 0; i < pairs.length; i++) {
-    const product = pairs[i][0] * pairs[i][1];
-    const lcmValue = lcm(pairs[i][0], pairs[i][1]);
-    result += product - lcmValue;
-  }
-
-  return result;
+  const gcd = (x, y) => (x == 0 ? y : gcd(y % x, x));
+  const lcm = (x, y) => (x * y == 0 ? 0 : (x * y) / gcd(x, y));
+  return pairs
+    .map((c) => c[0] * c[1] - lcm(c[0], c[1]))
+    .reduce((a, c) => a + c, 0);
 }
 
 console.log(
