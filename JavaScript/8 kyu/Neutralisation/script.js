f9888529d0acd49bb497c0fbb4f7405c8e98e100
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2023-11-08 11:31:54 +0000

    Complete solution

---
+++ JavaScript/8 kyu/Neutralisation/script.js
@@ -0,0 +1,42 @@
+/*
+Given two strings comprised of + and -, return a new string which shows how the two strings interact in the following way:
+
+When positives and positives interact, they remain positive.
+When negatives and negatives interact, they remain negative.
+But when negatives and positives interact, they become neutral, and are shown as the number 0.
+Worked Example
+("+-+", "+--") ➞ "+-0"
+# Compare the first characters of each string, then the next in turn.
+# "+" against a "+" returns another "+".
+# "-" against a "-" returns another "-".
+# "+" against a "-" returns "0".
+# Return the string of characters.
+Examples
+("--++--", "++--++") ➞ "000000"
+
+("-+-+-+", "-+-+-+") ➞ "-+-+-+"
+
+("-++-", "-+-+") ➞ "-+00"
+Notes
+The two strings will be the same length.
+*/
+
+const neutralise = (s1, s2) => {
+  if (s1.length !== s2.length) {
+    return "Input strings must have the same length.";
+  }
+  let result = "";
+  for (let i = 0; i < s1.length; i++) {
+    if (s1[i] === "+" && s2[i] === "+") {
+      result += "+";
+    } else if (s1[i] === "-" && s2[i] === "-") {
+      result += "-";
+    } else {
+      result += "0";
+    }
+  }
+  return result;
+};
+
+console.log(neutralise("--++--", "++--++"));
+//000000
