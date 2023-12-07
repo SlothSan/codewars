2cb1204b45e1aa95f2a4ca2153edc4d745afde9f
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2023-12-07 11:15:34 +0000

    Complete solution

---
+++ JavaScript/7 kyu/Change two-dimensional array/script.js
@@ -0,0 +1,56 @@
+/*
+Function receive a two-dimensional square array of random integers. On the main diagonal, all the negative integers must be changed to 0, while the others must be changed to 1 (Note: 0 is considered non-negative, here).
+
+(You can mutate the input if you want, but it is a better practice to not mutate the input)
+
+Example:
+
+Input array
+
+[
+  [-1,  4, -5, -9,  3 ],
+  [ 6, -4, -7,  4, -5 ],
+  [ 3,  5,  0, -9, -1 ],
+  [ 1,  5, -7, -8, -9 ],
+  [-3,  2,  1, -5,  6 ]
+]
+
+Output array
+
+[
+  [ 0,  4, -5, -9,  3 ],
+  [ 6,  0, -7,  4, -5 ],
+  [ 3,  5,  1, -9, -1 ],
+  [ 1,  5, -7,  0, -9 ],
+  [-3,  2,  1, -5,  1 ]
+]
+*/
+
+const matrix = (array) => {
+  let arrCopy = [...array];
+  for (let i = 0; i < arrCopy.length; i++) {
+    let curr = arrCopy[i][i];
+    curr >= 0 ? (arrCopy[i][i] = 1) : (arrCopy[i][i] = 0);
+  }
+  return arrCopy;
+};
+
+console.log(
+  matrix([
+    [-1, 4, -5, -9, 3],
+    [6, -4, -7, 4, -5],
+    [3, 5, 4, -9, -1],
+    [1, 5, -7, -8, -9],
+    [-3, 2, 1, -5, 6],
+  ])
+);
+
+/*
+[
+  [ 0, 4, -5, -9, 3 ],
+  [ 6, 0, -7, 4, -5 ],
+  [ 3, 5, 1, -9, -1 ],
+  [ 1, 5, -7, 0, -9 ],
+  [ -3, 2, 1, -5, 1 ]
+]
+*/
