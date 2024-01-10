3d98e8a8f7d877769cc0619e47c63b768e0f9ded
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2024-01-10 09:29:20 +0000

    Remove comment .

--- JavaScript/6 kyu/Free pizza/script.js
+++ JavaScript/6 kyu/Free pizza/script.js
@@ -20,8 +20,8 @@ const pizzaRewards = (customers, minOrders, minPrice) => {
 let minOrders = 5;
 let minPrice = 20;
 let customers = {
-  "John Doe": [22, 30, 11, 17, 15, 52, 27, 12], // Only has three orders above 20$, so no pizza
-  "Jane Doe": [5, 17, 30, 33, 40, 22, 26, 10, 11, 45], // Has six orders above 20$, which means FREE PIZZA!
+  "John Doe": [22, 30, 11, 17, 15, 52, 27, 12],
+  "Jane Doe": [5, 17, 30, 33, 40, 22, 26, 10, 11, 45],
 };
 
 console.log(pizzaRewards(customers, minOrders, minPrice)); //[ 'Jane Doe' ]
