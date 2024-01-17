8002ddb3c4c4e3ff1246931e8aca185e4657d01b
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2024-01-17 12:38:31 +0000

    Compelte solution.

---
+++ JavaScript/7 kyu/Remove HTML tags using regexp/script.js
@@ -0,0 +1,19 @@
+/*
+Remove HTML tags and return string without:
+<tag> and </tag>
+<tag/>
+<tag />
+html tags with attributes.
+Don't trim space, tab etc.
+You have to use regexp.
+
+Tests are using function:
+String.prototype.replace(your regexp, "")
+
+Have fun :)
+*/
+
+const reg = /<[^>]+>|<\/[^>]+>|<[^\/]+\/>/g;
+
+console.log("<div>TESTING!23</div>".replace(reg, ""));
+//TESTING!23
