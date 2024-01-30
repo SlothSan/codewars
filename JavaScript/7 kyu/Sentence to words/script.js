f3cf6123681dbf51ba764e0434a206a35e869fb5
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2024-01-30 08:33:21 +0000

    Complete solution.

---
+++ JavaScript/7 kyu/Sentence to words/script.js
@@ -0,0 +1,11 @@
+/*
+Write function splitSentence which will create a list of strings from a string.
+
+Example:
+
+"hello world" -> ["hello", "world"]
+*/
+
+const splitSentence = (s) => s.split(" ");
+
+console.log(splitSentence("Hello World!")); //[ 'Hello', 'World!' ]
