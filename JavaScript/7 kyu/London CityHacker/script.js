5756a03f7d36f82a498b1d133bd3a91b5b391b66
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2024-01-31 21:10:18 +0000

    Complete solution

---
+++ JavaScript/7 kyu/London CityHacker/script.js
@@ -0,0 +1,33 @@
+/*
+You are given a sequence of a journey in London, UK. The sequence will contain bus numbers and TFL tube names as strings e.g.
+
+['Northern', 'Central', 243, 1, 'Victoria']
+
+Journeys will always only contain a combination of tube names and bus numbers. Each tube journey costs £2.40 and each bus journey costs £1.50. If there are 2 or more adjacent bus journeys, the bus fare is capped for sets of two adjacent buses and calculated as one bus fare for each set.
+
+For example: 'Piccadilly', 56, 93, 243, 20, 14 -> "£6.90"
+
+Your task is to calculate the total cost of the journey and return the cost rounded to 2 decimal places in the format (where x is a number): £x.xx
+*/
+
+const londonCityHacker = (journey) => {
+  let totalCost = 0;
+  const TubeCost = 2.4;
+  const BusCost = 1.5;
+  let conseq = false;
+  for (let i = 0; i < journey.length; i++) {
+    let curr = journey[i];
+    if (typeof curr === "string") {
+      totalCost += TubeCost;
+    } else {
+      let next = journey[i + 1];
+      if (typeof next === "number" && !conseq) {
+        conseq = true;
+      } else {
+        totalCost += BusCost;
+        conseq = false;
+      }
+    }
+  }
+  return `£${totalCost.toFixed(2)}`;
+};
