2f33b9f793a1eba5a453ca78178167ea348310bd
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2024-02-13 10:04:09 +0000

    Complete solution.

---
+++ JavaScript/6 kyu/Look and say sequence generator/script.js
@@ -0,0 +1,39 @@
+/*
+The look and say sequence is a sequence in which each number is the result of a "look and say" operation on the previous element.
+
+Considering for example the classical version startin with "1": ["1", "11", "21, "1211", "111221", ...]. You can see that the second element describes the first as "1(times number)1", the third is "2(times number)1" describing the second, the fourth is "1(times number)2(and)1(times number)1" and so on.
+
+Your goal is to create a function which takes a starting string (not necessarily the classical "1", much less a single character start) and return the nth element of the series.
+
+Examples
+lookAndSaySequence("1", 1)   === "1"
+lookAndSaySequence("1", 3)   === "21"
+lookAndSaySequence("1", 5)   === "111221"
+lookAndSaySequence("22", 10) === "22"
+lookAndSaySequence("14", 2)  === "1114"
+Trivia: "22" is the only element that can keep the series constant.
+*/
+
+const generateNext = (str) => {
+  let result = "";
+  let count = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else {
+      result += count + str[i];
+      count = 1;
+    }
+  }
+  return result;
+};
+
+const lookAndSaySequence = (firstElement, n) => {
+  let result = firstElement;
+  for (let i = 1; i < n; i++) {
+    result = generateNext(result);
+  }
+  return result;
+};
+
+console.log(lookAndSaySequence("22", 10)); //22
