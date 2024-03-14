4b323d3e42d08d3423a2bfee6004200aed25ad58
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2024-03-14 17:09:46 +0000

    Complete solution.

---
+++ JavaScript/2 kyu/Multi Line Task++ Hello World/script.js
@@ -0,0 +1,38 @@
+/*
+You need to write a function f that returns the string Hello, world!.
+
+Requirement: Every line must have at most 2 characters, and total number of lines must be less than 40.
+
+Hint: It's possible to complete this in 28 lines only.
+*/
+
+f = 
+""
+[
+"\
+t\
+r\
+i\
+m"
+][
+  "\
+b\
+i\
+n\
+d"
+]`
+H\
+e\
+l\
+l\
+o\
+,\
+ \
+w\
+o\
+r\
+l\
+d\
+!`;
+
+console.log(f()); //Hello, world!
