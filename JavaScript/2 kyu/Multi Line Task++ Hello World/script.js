2676f8d1f0a307d4b7d139b1754e2c47f2d3ffaf
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2024-03-14 17:10:22 +0000

    add comments.

--- JavaScript/2 kyu/Multi Line Task++ Hello World/script.js
+++ JavaScript/2 kyu/Multi Line Task++ Hello World/script.js
@@ -6,6 +6,9 @@ Requirement: Every line must have at most 2 characters, and total number of line
 Hint: It's possible to complete this in 28 lines only.
 */
 
+//PS this is disgusting and code golf is all sorts of wrong. 
+// I wrote this and I hate it. 
+
 f = 
 ""
 [
