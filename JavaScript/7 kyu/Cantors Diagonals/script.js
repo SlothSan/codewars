7143259fd017a20f950e807ed1295c04f5a6800d
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2024-05-08 09:51:56 +0100

    Complete solution.

---
+++ JavaScript/7 kyu/Cantors Diagonals/script.js
@@ -0,0 +1,32 @@
+/*
+Given a list of lists containing only 1s and 0s, return a new list that differs from list 1 in its first element, list 2 in its second element, list 3 in its 3rd element, and so on.
+
+cantor([[0,0,0],
+        [1,1,1],
+        [0,1,0]]) = [1,0,1]
+
+cantor([[1,0,0],
+        [0,1,0],
+        [0,0,1]]) = [0,0,0]
+The nested list will always be perfectly square. Your solution should be a list containing only 1s and 0s.
+
+See Wikipedia for background (if you're interested; it won't help you solve the kata). Obviously this kata is not the same because the lists are not infinite so it doesn't really prove anything -- consider it a tribute...
+*/
+
+const cantor = (nestedList) => {
+  let result = [];
+  for (let i = 0; i < nestedList.length; i++) {
+    let curr = nestedList[i][i];
+    result.push(curr ? 0 : 1);
+  }
+  return result;
+};
+
+console.log(
+  cantor([
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 1, 0],
+  ])
+);
+//[ 1, 0, 1 ]
