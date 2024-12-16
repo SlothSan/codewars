693182f10605bedc010974955d524c0e6d0c10ac
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2024-12-16 08:37:46 +0000

    Complete solution.

---
+++ JavaScript/7 kyu/Manipulate URL Parameters/sctipt.js
@@ -0,0 +1,31 @@
+/*
+You need to write a function ( addOrChangeUrlParameter(url, keyValueParameter) ) that can manipulate URL parameters.
+
+It should be able to
+
+add a parameter to an existing URL,
+but also to
+
+change a parameter if it already exists.
+Example:
+
+addOrChangeUrlParameter("www.example.com", "key=value") 
+// -> 'www.example.com?key=value' (adds a parameter).
+
+addOrChangeUrlParameter("www.example.com?key=value", "key2=value2" ) 
+// -> 'www.example.com?key=value&key2=value2' (adds another parameter).
+
+addOrChangeUrlParameter("www.example.com?key=oldValue`", "key=newValue" ) 
+// ->'www.example.com?key=newValue' (changes the parameter).
+*/
+
+const addOrChangeUrlParameter = (url, param) => {
+  url = url.replace(new RegExp(param.split("=")[0] + "=[^&]*"), param);
+  return (
+    url +
+    (url.indexOf(param) >= 0 ? "" : (url.indexOf("?") >= 0 ? "&" : "?") + param)
+  );
+};
+
+console.log(addOrChangeUrlParameter("http://example.com/", "ID=1234"));
+//http://example.com/?ID=1234
