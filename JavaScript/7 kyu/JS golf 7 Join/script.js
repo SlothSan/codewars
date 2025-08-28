b1380e131401f9d0ebcf15c09fdb4320c2227def
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2025-08-28 15:33:21 +0100

    Complete solution.

---
+++ JavaScript/7 kyu/JS golf 7 Join/script.js
@@ -0,0 +1,12 @@
+/*
+  JS Golfing Series  
+Task:
+
+Return an array as string with no more than 13 characters. The delimiter should be an empty string, e.g. [123, "456"] should be stringified into "123456"
+
+Note that: undefined and null are shown as empty string.
+*/
+
+x = (a) => a.join``;
+
+console.log(x([1, 2, 3, 4, 5]));
