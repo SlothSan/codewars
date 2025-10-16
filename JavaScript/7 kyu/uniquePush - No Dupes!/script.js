daf6721646f5dbc5792bbb486e9fffbb7492416d
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2025-10-16 21:06:34 +0100

    Complete solution

---
+++ JavaScript/7 kyu/uniquePush - No Dupes!/script.js
@@ -0,0 +1,24 @@
+/*
+Write a function that will take an array and a person object as parameters. The function will only push a "person" object onto the end of an array if someone with that phone number doesn't already exist in that array.
+
+-A "person" is a javascript object with a name and a phoneNumber : {name:'SomeName', phoneNumber:1234567890}
+-A duplicate person object is an object with the same phoneNumber as someone else
+
+If the person object is unique, push them onto the end of the array, and return true.
+If the person object is NOT unique, don't push them to the array and return false;
+If the person doesn't have a phoneNumber, don't add them to the array and return false.
+*/
+
+const uniquePush = (arr, person) => {
+  if (!person || typeof person.phoneNumber === "undefined") {
+    return false;
+  }
+
+  const exists = arr.some((p) => p.phoneNumber === person.phoneNumber);
+  if (exists) {
+    return false;
+  }
+
+  arr.push(person);
+  return true;
+};
